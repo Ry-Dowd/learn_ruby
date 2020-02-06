8a33a0ad31fde0b991038b8c93f56270881c11fe
Ryan <rydowd@gmail.com>
2020-02-06 12:47:37 -0500

    wrote hello and temp, wrote functions and tests for calc

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+def add(number1, number2)
+  return number1 + number2
+end
+
+def subtract(number1, number2)
+  return number1 - number2
+end
+
+def sum(array)
+  total = 0
+  array.each{|x| total += x}
+  return total
+end
+
+def multiply(*args)
+  return args.reduce(:*)
+end
+
+def power(number, exponent)
+  total = 1
+  exponent.times{total *= number}
+  return total
+end
+
+def factorial(number)
+  if number > 0
+    return (1..number).inject(:*)
+  else
+    return 0
+  end
+end
+    
