f95c6b5f6880420b88894a3a1a3fbb6833e860bb
Ryan <rydowd@gmail.com>
2020-02-07 18:18:11 -0500

    added all features except the short-words list for simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -2,3 +2,27 @@
 def echo(word)
   return word
 end
+
+def shout(word)
+  return word.upcase
+end
+
+def repeat(word, times = 2)
+  return ([word]*times).join(" ")
+end
+
+def start_of_word(word, letters)
+  return word[0..(letters-1)]
+end
+
+def first_word(sentence)
+  return sentence.split[0]
+end
+
+def titleize(sentence)
+  words = sentence.split
+  p words
+  capital_words = words.map{ |x| x.capitalize }
+  p capital_words
+  return capital_words.join(" ")
+end
